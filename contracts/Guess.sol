73fea779cd7682623ccb7009311d5e7c354101cd
William Kim <wtk219@nyu.edu>
2017-06-19 15:45:06 -0400

    Very very basic layout

--- contracts/Guess.sol
+++ contracts/Guess.sol
@@ -1,6 +1,24 @@
 pragma solidity ^0.4.4;
 
+import "./MetaCoin.sol";
+
 contract Guess {
 
+    // Constructor, gives this contract 10k coins
+    function Guess(){
+        // MetaCoin(); //I think this should give the contract 10k coins
+    }
+    /* Intended behavior:
+        This contract should own 10,000 MetaCoin upon construction
+        Has a static array of 10 elements with numbers [1,10]
+
+        Users are gifted 100 coins upon sign-up.
+        Users can guess a number, if they get it right, they are gifted 10 coins
+        Lose 1 coin for each incorrect guess. (Check balance before I guess!)
+
+        This contract should be the sole initiator of MetaCoin, nobody else can
+        mint MetaCoin.
+
+    */
 
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
+// var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Guess = artifacts.require("./Guess.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+  deployer.link(MetaCoin, Guess);
+  deployer.deploy(Guess);
 };
